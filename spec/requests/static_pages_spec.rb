74a5e774dbb61b9e2a5c1627d83834db6d1bb811
Nikolay Nagornyy <n.nagornyy@gmail.com>
2018-03-19 01:37:36 +0300

    rspec DRY

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,47 +2,29 @@ require 'rails_helper'
 
 describe "Static pages" do
 
+  let(:title_peace){'Todo App'}
 
-  describe "Домашняя страница" do
+  page_set = ['home','contact','about']
 
-    it "Должна содержать тайтл " do
-      visit 'static_page/home'
-      expect(page).to have_title('Home')
+  page_set.each{|cur_page|
+
+    describe "Страница - #{cur_page}" do
+
+      it "Должна содержать тайтл " do
+        visit "static_page/#{cur_page}"
+        expect(page).to have_title("#{cur_page.capitalize}")
+      end
+
+      it "Должна содержать h1 " do
+        visit "static_page/#{cur_page}"
+        expect(page).to have_content("#{title_peace} | #{cur_page.capitalize}")
+      end
+
     end
 
-    it "Должна содержать h1 " do
-      visit 'static_page/home'
-      expect(page).to have_content('Home')
-    end
+  }
 
-  end
 
-  describe "Старница контакты" do
 
-    it "Должно содержать тайтл " do
-      visit 'static_page/contact'
-      expect(page).to have_title('Contact')
-    end
-
-    it "Должно содержать h1 " do
-      visit 'static_page/contact'
-      expect(page).to have_content('Contact')
-    end
-
-  end
-
-  describe "О нас" do
-
-    it "Должна содержать тайтл " do
-      visit 'static_page/about'
-      expect(page).to have_title('About')
-    end
-
-    it "Должна содержать h1 " do
-      visit 'static_page/about'
-      expect(page).to have_content('About')
-    end
-
-  end
 
 end
