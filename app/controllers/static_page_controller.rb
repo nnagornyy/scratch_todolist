c753f58e2cb507403e44074ebf9fe06ce8163215
Nikolay Nagornyy <n.nagornyy@gmail.com>
2018-03-18 22:48:46 +0300

    static page controller create

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,10 @@
+class StaticPageController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static_page/home'
+
+  get 'static_page/help'
+
+  get 'static_page/contact'
+
   resources :tasks
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
